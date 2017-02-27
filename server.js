29d2a7f1a0d7952b718c9f80c17c3c019dd5e474
Tom Barton <trb1g14@soton.ac.uk>
2017-02-27 17:59:39 +0000

    Added options to not inline images or compress

--- server.js
+++ server.js
@@ -8,33 +8,38 @@ app.use(bodyParser.json());
 
 // Initialize the app.
 var server = app.listen(process.env.PORT || 7300, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
 
 
 // CONTACTS API ROUTES BELOW
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
 }
 
 app.get("/", function (req, res) {
-    res.json({message: 'Hello World!'});
+  res.json({message: 'Hello World!'});
 
 });
 
 app.post("/api", function (req, res) {
-    var url = req.body.url;
-    console.log(url);
-    new Inliner(url, function (error, html) {
-        if (error) {
-            console.log(error);
-            res.status(500).json({"status": "error"});
-        } else {
-            // compressed and inlined HTML page
-            res.status(200).json({"status": "ok", "html": html});
-        }
-    });
+  var url = req.body.url;
+  console.log(url);
+  var opts = {
+    images: false,
+    compressCSS: true,
+    compressJS: true
+  };
+  new Inliner(url, opts, function (error, html) {
+    if (error) {
+      console.log(error);
+      res.status(500).json({"status": "error"});
+    } else {
+      // compressed and inlined HTML page
+      res.status(200).json({"status": "ok", "html": html});
+    }
+  });
 });
