4a4b7dd5045486c49d1d3df99e090103e5c2140d
Tom Barton <trb1g14@soton.ac.uk>
2017-03-02 17:42:29 +0000

    Simplified server to simply return the page

--- server.js
+++ server.js
@@ -1,49 +1,23 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var Inliner = require('inliner');
-var cors = require('cors');
+var express = require('express');
 
 var app = express();
+var bodyParser = require('body-parser');
+var Inliner = require('inliner');
+
 app.use(bodyParser.json());
-app.use(cors());
 
-
-// Initialize the app.
-var server = app.listen(process.env.PORT || 7300, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+app.get('/notes', function(req, res) {
+    res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 });
 
-
-// CONTACTS API ROUTES BELOW
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
-}
-
-app.get("/", function (req, res, next) {
-    res.json({message: 'Hello World!'});
-
-});
-
-app.post("/api", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    var url = req.body.url;
-    console.log(url);
-    var opts = {
-        images: false,
-        compressCSS: true,
-        compressJS: true
-    };
-    new Inliner(url, opts, function (error, html) {
-        if (error) {
-            console.log(error);
-            res.status(500).json({"status": "error"});
-        } else {
-            // compressed and inlined HTML page
-            res.status(200).json({"status": "ok", "html": html});
-        }
+app.get('/api', function(req, res) {
+    new Inliner('http://bbc.co.uk/news/popular/read', function (error, html) {
+        // compressed and inlined HTML page
+        console.log(error);
+        console.log(html);
+        res.send(html)
     });
+
 });
+
+app.listen(3000);
